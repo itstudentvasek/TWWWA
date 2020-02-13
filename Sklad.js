4aa855cb71e2d0a83fe1f60d20eb40e554e34d30
itstudentvasek <itstudentvasek@gmail.com>
2020-02-13 21:05:01 +0100

    Create Sklad.js
    přidána třída Sklad s konstruktorem ve kterém jsou nastavené vazby na html tagy

---
+++ Sklad.js
@@ -0,0 +1,15 @@
+class Sklad {
+
+    constructor() {
+               
+        this.kodInput = document.getElementById("kod");
+        this.tiskarnaInput = document.getElementById("tiskarna");
+        this.pocetInput = document.getElementById("pocet");
+        this.potvrditButton = document.getElementById("potvrdit");
+        this.vypisElement = document.getElementById("seznam-toneru");
+                 
+    }          
+               
+      
+
+}
