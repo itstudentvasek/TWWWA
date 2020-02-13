055cb2559fa163ea915d0d883d1e7b1311753383
itstudentvasek <itstudentvasek@gmail.com>
2020-02-13 22:14:53 +0100

    Update Sklad.js
    přidána metoda vypisTonery, která vypíše seznam tonerů

--- Sklad.js
+++ Sklad.js
@@ -19,11 +19,26 @@ class Sklad {
                                         const toner = new Toner(this.kodInput.value, this.tiskarnaInput.value, this.pocetInput.value);
                                         this.tonery.push(toner);
                                         this.ulozTonery();
-                                        
+                                        this.vypisTonery();
                                 } else
                                         alert("Musíte vyplnit všechny tři údaje!");
                                 };
     }
+    
+    vypisTonery() {                   
+                this.vypisElement.innerHTML = "";
+                for (const toner of this.tonery) {                       
+                                         
+                        const radek = document.createElement("span");
+                                                  
+                        radek.innerHTML += `<mark>${toner.kod}</mark> <strong>${toner.tiskarna}</strong> <em>počet: ${toner.pocet}</em>`;
+                        
+                        this.vypisElement.appendChild(radek);       
+                        
+                        const konecRadku = document.createElement("br");
+                        this.vypisElement.appendChild(konecRadku);
+                }
+     }
                
         ulozTonery() {
                 localStorage.setItem("tonery", JSON.stringify(this.tonery));
