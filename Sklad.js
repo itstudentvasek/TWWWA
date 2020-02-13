4902073cac8aa8be32557d47e6b348c221914031
itstudentvasek <itstudentvasek@gmail.com>
2020-02-13 21:09:03 +0100

    Update Sklad.js
    do konstruktoru přidáno načítání z localStorage

--- Sklad.js
+++ Sklad.js
@@ -1,15 +1,18 @@
 class Sklad {
 
     constructor() {
-               
+        const toneryZeStorage = localStorage.getItem("tonery");
+        this.tonery = toneryZeStorage ? JSON.parse(toneryZeStorage) : [];
+        
         this.kodInput = document.getElementById("kod");
         this.tiskarnaInput = document.getElementById("tiskarna");
         this.pocetInput = document.getElementById("pocet");
         this.potvrditButton = document.getElementById("potvrdit");
         this.vypisElement = document.getElementById("seznam-toneru");
-                 
+         
+        
     }          
-               
-      
-
+       
+       
+       
 }
