a1d588afe1e245110c1007723bed3714ea25f7bf
itstudentvasek <itstudentvasek@gmail.com>
2020-02-14 13:09:29 +0100

    Update start.js
    nastavení canvas - nadpis aplikace

--- start.js
+++ start.js
@@ -1,2 +1,15 @@
 const sklad = new Sklad();
 sklad.vypisTonery(); 
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "50px Arial";
+
+var grd = ctx.createLinearGradient(200, 0, 600, 0);
+grd.addColorStop(0, "blue");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.textAlign = "center"; 
+ctx.fillText("SEZNAM TONERŮ ",canvas.width/2, 70);
+
